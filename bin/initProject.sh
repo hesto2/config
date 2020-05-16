bfd19231b53d581935e917a49d8a963bb2390b9a
Carter Hesterman <carter.hesterman@lifeomic.com>
2020-05-16 16:46:33 -0600

    Updated gitignore to work properly

--- bin/initProject.sh
+++ bin/initProject.sh
@@ -44,7 +44,7 @@ echo 'module.exports = {...require("@hesto2/config/prettier.config.js")}' >prett
 
 npx json -I -f package.json -e 'this.husky={ "hooks": { "pre-commit": "pretty-quick --staged" } }'
 
-if [ $ciValue == 1 ] then
+if [ $ciValue == 1 ]; then
   cp -r ./node_modules/@hesto2/config/.circleci ./
 else
   cp ./node_modules/@hesto2/config/.travis.yml /
