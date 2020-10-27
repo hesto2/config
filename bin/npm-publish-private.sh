8578df40d046eed737c222bae3c74bd33dddc24a
hesto2 <hestermancarter@gmail.com>
2020-10-27 17:22:23 -0600

    update

--- bin/npm-publish-private.sh
+++ bin/npm-publish-private.sh
@@ -2,4 +2,5 @@
 echo starting publish
 npx npm-cli-login
 cp ~/.npmrc ~/project/.npmrc
-npm publish --access=private
+cd ~/project
+npm publish
--- bin/npm-publish.sh
+++ bin/npm-publish.sh
@@ -2,4 +2,5 @@
 echo starting publish
 npx npm-cli-login
 cp ~/.npmrc ~/project/.npmrc
+cd ~/project
 npm publish --access=public
